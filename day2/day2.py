f8ffdbab03c94e1dd1fe54a353d9ba506bdc7789
Abosh Upadhyaya <abosh@Aboshs-MacBook-Air.local>
2021-12-02 00:24:15 -0800

    completed day two

---
+++ day2/day2.py
@@ -0,0 +1,27 @@
+with open('input.txt') as f:
+    lines = [line.split() for line in f]
+
+# Part One
+position, depth = 0, 0
+for line in lines:
+    if line[0] == 'forward':
+        position += int(line[1])
+    elif line[0] == 'down':
+        depth += int(line[1])
+    else:
+        depth -= int(line[1])
+
+print(position * depth)
+
+# Part Two
+position, depth, aim = 0, 0, 0
+for line in lines:
+    if line[0] == 'forward':
+        position += int(line[1])
+        depth += aim * int(line[1])
+    elif line[0] == 'down':
+        aim += int(line[1])
+    else:
+        aim -= int(line[1])
+
+print(position * depth)

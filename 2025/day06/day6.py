6638bd08c9fd03db7a9f9df043895bf565a7d2f7
Abosh Upadhyaya <aboshu@gmail.com>
2025-12-06 00:52:11 -0800

    [2025] day 6 ⭐️⭐️

---
+++ 2025/day06/day6.py
@@ -0,0 +1,70 @@
+import math
+
+with open("./input.txt") as f:
+    lines = [line.strip().split() for line in f.readlines()]
+    ops = lines.pop()
+    lines = [[int(n) for n in line] for line in lines]
+
+# Part One
+total = 0
+for i, op in enumerate(ops):
+    if op == "*":
+        res = 1
+    else:
+        assert op == "+"
+        res = 0
+    for j in range(len(lines)):
+        n = lines[j][i]
+        if op == "*":
+            res *= n
+        else:
+            res += n
+
+    total += res
+
+print(total)
+
+# Part Two
+with open("./input.txt") as f:
+    lines = [line[:-1] for line in f.readlines()]
+    ops = lines.pop().split()
+
+
+column_ends = [len(lines[0])]
+c = len(lines[0]) - 1
+while c > 0:
+    # check if there's a value in the column
+    for r in range(len(lines)):
+        if lines[r][c] != " ":
+            c -= 1
+            break
+    else:
+        column_ends.insert(0, c)
+        c -= 1
+column_ends.insert(0, 0)  # dummy to make it ez
+
+matrix = []
+for c_i, c_e in enumerate(column_ends):
+    transposed = []
+    c = c_e - 1
+    while c >= column_ends[c_i - 1]:
+        n = 0
+        for i in range(len(lines)):
+            if lines[i][c] != " ":
+                n *= 10
+                n += int(lines[i][c])
+        if n > 0:
+            transposed.append(n)
+        c -= 1
+    matrix.append(transposed)
+matrix.pop(0)  # pop the dummy
+
+total = 0
+for op, nums in zip(ops, matrix):
+    if op == "*":
+        res = math.prod(nums)
+    else:
+        res = sum(nums)
+    total += res
+
+print(total)
